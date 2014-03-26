bffd41edde1037f562c374d0507c9272e8c7dc3b
RileyCampbell73 <riley.campbell73@gmail.com>
2014-03-26 17:05:24 -0400

    Start of making callbacks work.
    Need to implement on the GUI once Jhaig finishes it.

---
+++ PigGame/PigLib/CallBackInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace PigLib
+{
+
+
+    [DataContract]
+    public class CallBackInfo
+    {
+        [DataMember]
+        public int DieRoll { get; private set; }
+        [DataMember]
+        public int TotalPoints { get; private set; }
+        [DataMember]
+        public int BankedPoints { get; private set; }
+
+
+        public CallBackInfo(int d, int t, int b)
+        {
+            DieRoll = d;
+            TotalPoints = t;
+            BankedPoints = b;
+        }
+    }
+}
--- PigGame/PigLib/Pig.cs
+++ PigGame/PigLib/Pig.cs
@@ -4,15 +4,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.ServiceModel;
+
 namespace PigLib
 {
-    public interface IPig
-    {
 
+    public interface ICallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void UpdateGui(CallBackInfo info);
+        
     }
 
+    [ServiceContract(CallbackContract = typeof(ICallback))]
+    public interface IPig
+    {
+        [OperationContract]
+        int RegisterForCallbacks();
+        [OperationContract(IsOneWay = true)]
+        void UnregisterForCallbacks(int id);
+    }
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
     public class Pig : IPig
     {
+        //dictionary for clients
+        private Dictionary<int, ICallback> clientCallbacks = new Dictionary<int, ICallback>();
+        private int nextCallbackId = 1;
+
+        // C'tor
+        public Pig()
+        {
+            Console.WriteLine("Creating a Pig!");
+        }
+
+        public int RegisterForCallbacks()
+        {
+            // Store the ICallback interface (client object) reference for 
+            // the client which is currently calling RegisterForCallbacks()
+            ICallback cb = OperationContext.Current.GetCallbackChannel<ICallback>();
+            clientCallbacks.Add(nextCallbackId, cb);
+
+            return nextCallbackId++;
+        }
+
+        public void UnregisterForCallbacks(int id)
+        {
+            clientCallbacks.Remove(id);
+        }
 
     }
 }
